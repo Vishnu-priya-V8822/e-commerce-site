0a7933d5bb3a49175f4266be9706107a59ef944a
Vishnupriya V <vishnupriyav8822@gmail.com>
2025-07-20 20:16:56 +0530

    updated

--- store/urls.py
+++ store/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.register_view, name='register'),
     path('', views.home, name='home'),
+    path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('product/<int:pk>/', views.product_detail, name='product_detail'),
